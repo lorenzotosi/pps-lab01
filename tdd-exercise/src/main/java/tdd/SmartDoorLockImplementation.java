4a94e3d36d6ae9a14488a2460c8e143be127d23b
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-02-24 11:19:24 +0100

    isLocked method + test

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImplementation.java
@@ -0,0 +1,51 @@
+package tdd;
+
+public class SmartDoorLockImplementation implements SmartDoorLock {
+
+    private boolean isLocked;
+
+    SmartDoorLockImplementation() {
+        this.isLocked = true;
+    }
+
+
+    @Override
+    public void setPin(int pin) {
+
+    }
+
+    @Override
+    public void unlock(int pin) {
+
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.isLocked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,5 +1,6 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,4 +10,19 @@ public class SmartDoorLockTest {
     public void todo() {
         assertTrue(true);
     }
+
+    private SmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    public void setUp() {
+        this.smartDoorLock = new SmartDoorLockImplementation();
+    }
+
+    @Test
+    void testSmartDoorStartsLocked() {
+        assertTrue(smartDoorLock.isLocked());
+    }
+
+
+
 }
